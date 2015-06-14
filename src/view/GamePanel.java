d2c58663a4cb54a60efe1401fe8e5d268308d21e
Kevin <kevinschuurmans@msn.com>
2015-06-14 21:12:06 +0200

    Implemented mouselistener

--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -3,19 +3,23 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 import model.Game;
 
 @SuppressWarnings("serial")
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements MouseListener {
 	
 	private static final Color BACKGROUND_COLOR = new Color(255, 255, 255);
 	
 	public GamePanel(Game game) {
 		setBackground(BACKGROUND_COLOR);
 		setLayout(new BorderLayout());
+		setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 		
 		add(new ScorePanel(game), BorderLayout.NORTH);
 		add(new GridPanel(game.getGrid()), BorderLayout.CENTER);
@@ -25,6 +29,36 @@ public class GamePanel extends JPanel {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
