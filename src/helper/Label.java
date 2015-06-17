416f4a4579d3ebda0fd9beead4aa5650a0ce6ff4
Kevin <kevinschuurmans@msn.com>
2015-06-17 19:06:56 +0200

    Changed score color

--- src/helper/Label.java
+++ src/helper/Label.java
@@ -9,11 +9,12 @@ import javax.swing.JLabel;
 @SuppressWarnings("serial")
 public class Label extends JLabel {
 
+	private static final Color COLOR_STEEL_BLUE = new Color(70,	130, 180);
 	private static final Font FONT = new Font("Arial", Font.BOLD, 22);
 	
 	public Label() {		
 		setFont(FONT);
-		setForeground(Color.BLUE);
+		setForeground(COLOR_STEEL_BLUE);
 	}
 
 	@Override
